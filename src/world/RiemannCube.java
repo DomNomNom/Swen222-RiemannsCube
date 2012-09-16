4280199c35a90fa3933f7d63b5768fb74dac1318
Dominik Schmid <DominikSchmid93@gmail.com>
2012-09-16 20:18:23 +1200

    Added base code for the game world

--- src/world/RiemannCube.java
+++ src/world/RiemannCube.java
@@ -1,5 +1,15 @@
 package world;
 
+import world.cubes.Cube;
+
 public class RiemannCube {
+  public Cube[][][] cubes;
   
+  public Cube[][] getSlice(int x, int y, int norm_x, int norm_y, int norm_z) {
+    int sliceWd = 0; // TODO
+    int sliceHt = 0;
+    Cube[][] slice = new Cube[sliceWd][sliceHt];
+    // TODO
+    return slice;
+  }
 }
