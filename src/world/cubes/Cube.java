13df9e376613804e2b63c245eef9082d4825aaf0
Dominik Schmid <DominikSchmid93@gmail.com>
2012-09-19 14:46:56 +1200

    Re-added Cube

---
+++ src/world/cubes/Cube.java
@@ -0,0 +1,31 @@
+package world.cubes;
+
+
+import world.objects.Player;
+import world.objects.GameObject;
+
+public abstract class Cube {
+
+    private GameObject object;
+    private Player player;
+
+    
+    abstract public int type();
+
+    
+    public void setObject(GameObject o){
+        object = o;
+    }
+    
+    public GameObject object(){
+        return object;
+    }
+
+    public Player player() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+}
