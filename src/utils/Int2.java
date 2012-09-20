2cc96bb71eb153eafe548faa32b034b07e14dd5e
Dominik Schmid <DominikSchmid93@gmail.com>
2012-09-21 10:04:15 +1200

    Made the vectors nicer

--- src/utils/Int2.java
+++ src/utils/Int2.java
@@ -1,10 +1,26 @@
 package utils;
 
+
+/**
+ * A very simple 2D integer vector.
+ * The source code is simple enough to be intuitive to anyone knowing about vectors 
+ * 
+ * @author schmiddomi
+ *
+ */
 public class Int2 {
+    
     public int x, y;
     
-    public Int2(int x, int y) {
-        this.x = x;
-        this.y = y;
+    
+    public Int2(int X, int Y) {
+        x = Y;
+        y = Y;
     }
+    
+    
+    public void add(Int2 o)              { x+=o.x;  y+=o.y; }
+    public void add(int X, int Y, int Z) { x+=  X;  y+=  Y; }
+    
+    public Int2 copy() { return new Int2(x, y); }
 }
--- src/utils/Int3.java
+++ src/utils/Int3.java
@@ -1,20 +1,25 @@
 package utils;
 
 /**
- * A very brief 3D integer vector
+ * A very simple 3D integer vector
+ * The code is simple enough to be intuitive to anyone knowing about vectors 
+ * 
  * @author schmiddomi
  *
  */
-public class Int3 extends Int2 {
+public class Int3 {
+    
     public int x, y, z;
     
     public Int3(int X, int Y, int Z) {
-        super(X, Y);
-        this.z = Z;
+        x = X;
+        y = Y;
+        z = Z;
     }
+    
 
-    public void add(int X, int Y, int Z) { x+=X; y+=Y; z+=Z; }
-    public void add(Int3 o) { x+=o.x; y+=o.y; z+=o.z; }
+    public void add(Int3 o)              { x+=o.x; y+=o.y; z+=o.z; }
+    public void add(int X, int Y, int Z) { x+=  X; y+=  Y; z+=  Z; }
     
     public Int3 copy() { return new Int3(x,y,z); }
 }
