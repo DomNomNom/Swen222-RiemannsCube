172b287ea7d93e07a5a5aba9d1e587d8a34cb7d8
David Saxon <david.saxon@windowslive.com>
2012-09-24 23:59:37 +1200

    moved glass to cubes

---
+++ src/world/cubes/Glass.java
@@ -0,0 +1,13 @@
+package world.cubes;
+
+
+public class Glass extends Cube {
+
+    public Glass(){ }
+    
+    @Override
+    public int type() {
+        return CubeType.GLASS.ordinal();
+    }
+
+}
