b96ed55f479dfeb400a1ae535784368bf3cfd70b
Jayden Mudge <jaydenmudge@gmail.com>
2012-09-28 10:10:35 +1200

    Adding doors and triggers to XML Parser tests.

--- src/tests/XMLParsingTests.java
+++ src/tests/XMLParsingTests.java
@@ -2,40 +2,88 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.awt.Color;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
-import java.io.IOException;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import data.LevelPipeline;
 import data.XMLParser;
 
-import utils.Int3;
 import world.RiemannCube;
+import world.items.Key;
+import world.objects.Door;
+import world.objects.Lock;
 import world.objects.Player;
+import world.objects.Trigger;
 
 public class XMLParsingTests {
 
-    RiemannCube riemann;
-    @Test
-    public void test() {
-        riemann = new RiemannCube(new Int3(2, 2, 2));
+
+    RiemannCube riemann = new RiemannCube(3, 3, 3);
+
+    LevelPipeline pipe = new LevelPipeline();
+    File file = new File("testCase.xml");
+    FileWriter filewriter;
+    
+    RiemannCube load;
+    
+    @Before
+    public void initMap(){
+
+        Color col = Color.PINK;
+        Door door = new Door(2, col);
+        
+        Trigger trig = new Lock(1, col);
+        door.addTrigger(1);
+        Key key = new Key(col);
+
+        riemann.getCube(0,1,2).addObject(door);
+        riemann.getCube(0,1,1).addObject(trig);
+        riemann.getCube(0,1,0).addObject(key);
+        
+        trig = new Lock(2, col);
+        
+        riemann.getCube(0,2,1).addObject(trig);
+        riemann.getCube(0,2,0).addObject(key);
+        
         Player p = new Player(riemann.getCube(0,0,0), 1);
         riemann.getCube(0,0,0).addObject(p);
-        LevelPipeline pipe = new LevelPipeline();
-        File file = new File("testCase.xml");
-        FileWriter filewriter;
+        
+
         try {
-           filewriter = new FileWriter(file);
-           pipe.save(riemann, filewriter);
-           RiemannCube load = XMLParser.readXML(file);
-           assert(load.cubes[0][0][0].player().id()==1);
-        } catch (Exception e) {
-            e.printStackTrace();
-            fail();
-            }
+            filewriter = new FileWriter(file);
+            pipe.save(riemann, filewriter);
+            load = XMLParser.readXML(file);
+            
+         } catch (Exception e) {
+             e.printStackTrace();
+             fail();
+         }
+    }
+    
+    
+    @Test
+    public void testPlayers() {
+       assert(load.cubes[0][0][0].player().id()==1);
+    }
+    
+    @Test
+    public void testObjectsLoading() {
+        assertTrue(load.cubes[0][1][2].object() instanceof Door);
+        assertTrue(load.cubes[0][1][1].object() instanceof Lock);
+    }
+    
+    @Test
+    public void testObjectsAssociating() {
+        Door door = (Door) load.cubes[0][1][2].object();
+        Lock lock = (Lock) load.cubes[0][1][1].object();
+        
+        if(door.triggers()!=null)
+                assertTrue(door.triggers().contains(lock.getID()));
+            else fail("Need to initialise door.triggers()");
     }
 
 }
