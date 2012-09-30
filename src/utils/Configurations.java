b71945a8e1d5e90f2610f6eade346e4a3aff5ab7
Sivan <fesherman@gmail.com>
2012-10-01 11:58:34 +1300

    Added a configuration class for global variables

---
+++ src/utils/Configurations.java
@@ -0,0 +1,15 @@
+package utils;
+
+/**
+ * A class that allows toggling of options in the game 
+ * for debugging purposes.
+ * All fields are public in order to allow all parts of the program the 
+ * ease of using this options for testing and debugging.
+ * Meant to provide a set of global variables.    
+ * @author feshersiva
+ */
+public class Configurations {
+    
+    public static boolean debugPrint = false; 
+    public static boolean highRes = false; 
+}
