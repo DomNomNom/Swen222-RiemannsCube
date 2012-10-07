d42ed73f5e78bb531b2584d209fc0bf8aaafafca
Dominik Schmid <DominikSchmid93@gmail.com>
2012-10-08 11:40:28 +1300

    Added a comment

--- src/tests/EventTests.java
+++ src/tests/EventTests.java
@@ -15,6 +15,9 @@ import world.events.FullStateUpdate;
 
 public class EventTests {
 
+    /**
+     * This simulates the server switching a level without all the server/client/networking complications
+     */
     @Test
     public void testFullStateUpdate() {
         RiemannCube world1 = WorldTests.generateWorld();
@@ -25,7 +28,7 @@ public class EventTests {
         
         FullStateUpdate event = new FullStateUpdate(out.toString());
         
-        // ==== Here the event would get sent over the network and received at the other client(s) ====
+        // ==== Here, the event would get sent over the network and received at the other client(s) ====
         
         RiemannCube world2 = XMLParser.readXML(new ByteArrayInputStream(event.level.getBytes()));
         assertTrue(world1.equals(world2));
