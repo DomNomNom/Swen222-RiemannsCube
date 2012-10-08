0b461f52904b39a024714a23d656020425c79266
Dominik Schmid <DominikSchmid93@gmail.com>
2012-10-09 10:06:53 +1300

    Added more tests, they currently do not pass

--- src/tests/ActionTests.java
+++ src/tests/ActionTests.java
@@ -2,12 +2,23 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.awt.Color;
+
 import org.junit.Test;
 
 import utils.Int3;
 import world.RiemannCube;
+import world.cubes.Cube;
+import world.events.Action;
+import world.events.ItemDrop;
+import world.events.ItemPickup;
+import world.events.ItemUse;
 import world.events.PlayerMove;
 import world.events.PlayerSpawning;
+import world.objects.Container;
+import world.objects.GameObject;
+import world.objects.items.GameItem;
+import world.objects.items.Key;
 
 public class ActionTests {
 
@@ -19,10 +30,12 @@ public class ActionTests {
         assertTrue(world.equals(world2));
         assertTrue(world2.applyAction(new PlayerMove(0, new Int3(1, 0, 0))));
         assertFalse(world.equals(world2));
+        assertTrue(world2.applyAction(new PlayerMove(0, new Int3(-1, 0, 0))));
+        assertTrue(world.equals(world2));
     }
     
     @Test
-    public void testSpawnAction() {        
+    public void testSpawnAction() {
         RiemannCube world  = WorldTests.generateWorld();
         RiemannCube world2 = WorldTests.generateWorld();
         
@@ -36,4 +49,58 @@ public class ActionTests {
         assertTrue(world.equals(world2));
     }
 
+    
+    @Test
+    public void testPickupDropUse() {
+        RiemannCube world  = WorldTests.generateWorld();
+        Cube pos = world.getCube(0,0,0);
+        
+        GameObject containter = new Container(pos, Color.GREEN, world);
+        GameItem key  = new Key(pos, Color.BLUE);
+        GameItem key2 = new Key(pos, Color.RED);
+        assertTrue(pos.addObject(key       ));
+        assertTrue(pos.addObject(key2      ));
+        assertTrue(pos.addObject(containter));
+        
+        // new we should have a player, 2 keys and a container in the same cube.
+        
+        // these are the actions we are going to test
+        Action pickup = new ItemPickup(0);
+        Action use    = new ItemUse   (0); // this is both pickup and put in
+        Action drop   = new ItemDrop  (0);
+
+        assertTrue (world.isValidAction(pickup));
+        assertFalse(world.isValidAction(use   ));
+        assertFalse(world.isValidAction(drop  ));
+        
+        assertTrue(world.applyAction(pickup)); // pick up a key
+
+        assertFalse(world.isValidAction(pickup));
+        assertTrue (world.isValidAction(use   ));
+        assertTrue (world.isValidAction(drop  ));
+        
+        assertTrue(world.applyAction(use)); // put the key in the container
+        
+        assertTrue (world.isValidAction(pickup));
+        assertTrue (world.isValidAction(use   ));
+        assertFalse(world.isValidAction(drop  ));
+        
+        assertTrue(world.applyAction(pickup)); // pick up second key
+        
+        assertFalse(world.isValidAction(pickup));
+        assertFalse(world.isValidAction(use   )); // we can't put it in the container as the container is full
+        assertTrue (world.isValidAction(drop  ));
+        
+        assertTrue(world.applyAction(drop)); // drop it again
+
+        assertTrue (world.isValidAction(pickup));
+        assertTrue (world.isValidAction(use   ));
+        assertFalse(world.isValidAction(drop  ));
+        
+        assertTrue(world.applyAction(use)); // get the key out of the container (picks it up)
+        
+        assertFalse(world.isValidAction(pickup));
+        assertTrue (world.isValidAction(use   ));
+        assertTrue (world.isValidAction(drop  ));
+    }
 }
